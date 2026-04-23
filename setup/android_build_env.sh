eba2aacae3b95dddf8c0d2c72657ad37b1b50956
Adarsh0127-Elite <adarshmoharana027@gmail.com>
2026-04-23 11:21:21 +0000

    setup: android_build_env: Update the script for Ubuntu24
    Signed-off-by: Adarsh0127-Elite <adarshmoharana027@gmail.com>

--- setup/android_build_env.sh
+++ setup/android_build_env.sh
@@ -7,17 +7,18 @@
 # Script to setup an AOSP Build environment on Ubuntu and Linux Mint
 
 LATEST_MAKE_VERSION="4.3"
-UBUNTU_16_PACKAGES="libesd0-dev"
-UBUNTU_20_PACKAGES="libncurses5 curl python-is-python3"
-DEBIAN_10_PACKAGES="libncurses5"
-DEBIAN_11_PACKAGES="libncurses5"
+UBUNTU_16_PACKAGES="libesd0-dev lib32ncurses5-dev libtinfo5 libncurses5-dev python2.7 python-all-dev"
+UBUNTU_20_PACKAGES="libncurses5 curl python-is-python3 lib32ncurses5-dev libtinfo5 libncurses5-dev python2.7 python-all-dev"
+UBUNTU_24_PACKAGES="curl lib32ncurses-dev libtinfo6 libncurses-dev python-is-python3 python-dev-is-python3 python3-dev"
+DEBIAN_10_PACKAGES="libncurses5 lib32ncurses5-dev libtinfo5 libncurses5-dev python2.7 python-all-dev"
+DEBIAN_11_PACKAGES="libncurses5 lib32ncurses5-dev libtinfo5 libncurses5-dev python2.7 python-all-dev"
 PACKAGES=""
 
 sudo apt install software-properties-common -y
 sudo apt update
 
-# Install lsb-core packages
-sudo apt install lsb-core -y
+# Install lsb_release provider
+sudo apt install lsb-release -y
 
 LSB_RELEASE="$(lsb_release -d | cut -d ':' -f 2 | sed -e 's/^[[:space:]]*//')"
 
@@ -25,6 +26,8 @@ if [[ ${LSB_RELEASE} =~ "Mint 18" || ${LSB_RELEASE} =~ "Ubuntu 16" ]]; then
     PACKAGES="${UBUNTU_16_PACKAGES}"
 elif [[ ${LSB_RELEASE} =~ "Ubuntu 20" || ${LSB_RELEASE} =~ "Ubuntu 21" || ${LSB_RELEASE} =~ "Ubuntu 22" || ${LSB_RELEASE} =~ 'Pop!_OS 2' ]]; then
     PACKAGES="${UBUNTU_20_PACKAGES}"
+elif [[ ${LSB_RELEASE} =~ "Ubuntu 24" ]]; then
+    PACKAGES="${UBUNTU_24_PACKAGES}"
 elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 10" ]]; then
     PACKAGES="${DEBIAN_10_PACKAGES}"
 elif [[ ${LSB_RELEASE} =~ "Debian GNU/Linux 11" ]]; then
@@ -36,11 +39,11 @@ sudo DEBIAN_FRONTEND=noninteractive \
     adb autoconf automake axel bc bison build-essential \
     ccache clang cmake curl expat fastboot flex g++ \
     g++-multilib gawk gcc gcc-multilib git git-lfs gnupg gperf \
-    htop imagemagick lib32ncurses5-dev lib32z1-dev libtinfo5 libc6-dev libcap-dev \
-    libexpat1-dev libgmp-dev '^liblz4-.*' '^liblzma.*' libmpc-dev libmpfr-dev libncurses5-dev \
-    libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils '^lzma.*' lzop \
+    htop imagemagick lib32z1-dev libc6-dev libcap-dev \
+    libexpat1-dev libgmp-dev lz4 liblz4-dev liblzma-dev libmpc-dev libmpfr-dev \
+    libsdl1.2-dev libssl-dev libtool libxml2 libxml2-utils lzma lzma-dev lzop \
     maven ncftp ncurses-dev patch patchelf pkg-config pngcrush \
-    pngquant python2.7 python3-pyelftools python-all-dev re2c schedtool squashfs-tools subversion \
+    pngquant python3-pyelftools re2c schedtool squashfs-tools subversion \
     texinfo unzip w3m xsltproc zip zlib1g-dev lzip \
     libxml-simple-perl libswitch-perl apt-utils rsync \
     ${PACKAGES} -y
@@ -53,10 +56,13 @@ sudo apt update
 sudo apt install -y gh
 
 echo -e "Setting up udev rules for adb!"
-sudo curl --create-dirs -L -o /etc/udev/rules.d/51-android.rules -O -L https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules
-sudo chmod 644 /etc/udev/rules.d/51-android.rules
-sudo chown root /etc/udev/rules.d/51-android.rules
-sudo systemctl restart udev
+if sudo curl --fail --retry 3 --connect-timeout 30 --create-dirs -L -o /etc/udev/rules.d/51-android.rules https://raw.githubusercontent.com/M0Rf30/android-udev-rules/master/51-android.rules; then
+    sudo chmod 644 /etc/udev/rules.d/51-android.rules
+    sudo chown root /etc/udev/rules.d/51-android.rules
+    sudo systemctl restart udev || sudo udevadm control --reload-rules
+else
+    echo "Failed to download adb udev rules"
+fi
 
 if [[ "$(command -v make)" ]]; then
     makeversion="$(make -v | head -1 | awk '{print $3}')"
@@ -67,5 +73,8 @@ if [[ "$(command -v make)" ]]; then
 fi
 
 echo "Installing repo"
-sudo curl --create-dirs -L -o /usr/local/bin/repo -O -L https://storage.googleapis.com/git-repo-downloads/repo
-sudo chmod a+rx /usr/local/bin/repo
+if sudo curl --fail --retry 3 --connect-timeout 30 --create-dirs -L -o /usr/local/bin/repo https://storage.googleapis.com/git-repo-downloads/repo; then
+    sudo chmod a+rx /usr/local/bin/repo
+else
+    echo "Failed to download repo"
+fi
